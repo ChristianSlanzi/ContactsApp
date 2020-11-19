6b6096e83f3cd2ec82b2e928c49dafd036870e45
Christian Slanzi <christian.slanzi@gmail.com>
2020-11-19 20:54:03 +0100

    add new project

---
+++ ContactsApp/ContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Christian Slanzi on 19.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
